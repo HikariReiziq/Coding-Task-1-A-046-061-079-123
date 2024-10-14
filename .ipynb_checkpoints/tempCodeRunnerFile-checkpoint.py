05e417b5310a78c91beb6ae1549604ee84bef646
HikariReiziq <reiziqzip@gmail.com>
2024-10-14 08:28:29 +0700

    First Commit
    Memasukkan fila dataset excel ke dalam Jupyter notebook

---
+++ .ipynb_checkpoints/tempCodeRunnerFile-checkpoint.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df=pd.read_excel(r"C:\Users\Hikari Reiziq\Project StatProb tugas coding",sheet_name="mpg")
+print(df)
---
+++ TugasStatProb.py
@@ -0,0 +1,5 @@
+import pandas
+dataset = pd.read.excel("C:\\Users\\Hikari Reiziq\\ProjectStatProbTugasCoding\\Coding-Task-1-A-046-061-079-123\\Dataset.xlsx")
+import pandas as pd
+dataset = pd.read_excel("Dataset.xlsx")
+dataset
